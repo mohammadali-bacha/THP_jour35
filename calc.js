bd3552cbae34bce98d5247d6772d55c6393f8210
MOHAMMAD-ALI <bacha@tuta.io>
2018-05-21 17:05:17 +0200

    1st commit

---
+++ calc.js
@@ -0,0 +1,38 @@
+var nb1 = 10;
+var nb2 = 2;
+var result = 0;
+
+// function my_max(){
+// }
+// function vowel_count(){
+// }
+// function reverse(){
+// Array = [];
+// Array.reverse;
+// }
+
+function add() {
+var r = nb1 + nb2;
+console.log(r);
+
+}
+
+function multiply() {
+    var r = nb1 * nb2;
+    console.log(r);
+}
+
+function divide() {
+    var r = nb1 / nb2;
+    console.log(r);
+}
+
+function substract() {
+    var r = nb1 - nb2;
+    console.log(r);
+}
+
+add();
+multiply();
+divide();
+substract();
