d1f056be530567fd48bd12d54d0cf39986fd8057
MOHAMMAD-ALI <bacha@tuta.io>
2018-05-22 23:40:47 +0200

    final

--- calc.js
+++ calc.js
@@ -1,30 +1,62 @@
-var nb1 = 10;
-var nb2 = 2;
-var result = 0;
+// var nb1 = 10;
+// var nb2 = 2;
+// var result = 0;
 
 
-function add() {
-var r = nb1 + nb2;
-console.log(r);
+// function add() {
+// var r = nb1 + nb2;
+// console.log(r);
 
+// }
+
+// function multiply() {
+//     var r = nb1 * nb2;
+//     console.log(r);
+// }
+
+// function divide() {
+//     var r = nb1 / nb2;
+//     console.log(r);
+// }
+
+// function substract() {
+//     var r = nb1 - nb2;
+//     console.log(r);
+// }
+
+// add();
+// multiply();
+// divide();
+// substract();
+
+function btnplus() {
+    Calculator.display.value += "+";
+
+    Calculator.display.style.textAlign = "right";
 }
 
-function multiply() {
-    var r = nb1 * nb2;
-    console.log(r);
+function btnsub() {
+    Calculator.display.value += "-";
+
+    Calculator.display.style.textAlign = "right";
 }
 
-function divide() {
-    var r = nb1 / nb2;
-    console.log(r);
+function btnmult() {
+    Calculator.display.value += "*";
+
+    Calculator.display.style.textAlign = "right";
 }
 
-function substract() {
-    var r = nb1 - nb2;
-    console.log(r);
+
+
+function btndiv() {
+    Calculator.display.value += "/";
+
+    Calculator.display.style.textAlign = "right";
 }
 
-add();
-multiply();
-divide();
-substract();
+function btnclear() {
+    Calculator.display.value = "";
+
+    Calculator.display.style.textAlign = "right";
+}
